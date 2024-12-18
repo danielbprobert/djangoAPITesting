af95394a870ba4ee1f5bb0e4458a2b24b844a9cc
Daniel Probert <danielbprobert@gmail.com>
2024-12-18 19:58:13 +0000

    media root configuration

--- hyphen8apis/settings.py
+++ hyphen8apis/settings.py
@@ -141,7 +141,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = '/home/ubuntu/djangoAPITesting/staticfiles'
 # Default primary key field type
